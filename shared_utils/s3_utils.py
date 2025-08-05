fa46edc373e6d18793cd728605943b7af469fb27
Ming Jiu <chiamingjiu@yahoo.com.sg>
2025-08-05 10:11:32 +0800

    fix: removed additional code that is not supposed to be present. fixes Ruff

--- shared_utils/s3_utils.py
+++ shared_utils/s3_utils.py
@@ -25,7 +25,7 @@ s3_client = boto3.client(
     "s3",
     endpoint_url=S3_ENDPOINT,
     aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY,message.content.strip().lower()
+    aws_secret_access_key=S3_SECRET_KEY,
     region_name=REGION_NAME,
 )
 redis_flag_store = RedisSimpleFileFlag()
