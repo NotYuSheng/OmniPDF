c52d8336242a773ed3742eeb1927e10e67df3cce
Lim Dong Wan <limdongwan201528@gmail.com>
2025-08-13 07:36:18 +0800

    chore: Add LLM_API_URL to openai_client

--- shared_utils/openai_client.py
+++ shared_utils/openai_client.py
@@ -7,6 +7,7 @@ def get_openai_client() -> AsyncOpenAI:
     Initialize and return an OpenAI client instance.
     """
     return AsyncOpenAI(
-        base_url=os.getenv("OPENAI_BASE_URL"),  # Make sure `/v1` is included
+        # base_url=os.getenv("OPENAI_BASE_URL"),  # Make sure `/v1` is included
+        base_url=os.getenv("LLM_API_URL"),
         api_key=os.getenv("OPENAI_API_KEY"),
     )
