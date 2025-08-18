fb5c492459a781c8e77898c4956eafbfd0acc121
mj <chiamingjiu@yahoo.com.sg>
2025-08-18 15:53:22 +0800

    fix: changed endpoint from get to post and added functionality to specify config nad page_info

--- pdf_processor_service/routers/embed.py
+++ pdf_processor_service/routers/embed.py
@@ -14,14 +14,21 @@ if not EMBED_URL:
     raise ValueError("EMBED_URL is not set")
 
 
-@router.get("/{embed_type}/{doc_id}")
+@router.post("/{embed_type}/{doc_id}")
 async def text_embed_proxy(
     embed_type: str,
     doc_id: str,
+    config: dict = {},
+    pages_info: list = [],
     session_id: str = Depends(get_session_id),
     _validated: bool = Depends(validate_session_doc_pair),
-    full_text: str = Depends(concat_text)
+    full_text: str = Depends(concat_text),
 ):
-    
-    param = {"doc_id": doc_id, "session_id": session_id, "text": full_text, "config": {}, "pages_info": []}
+    param = {
+        "doc_id": doc_id,
+        "session_id": session_id,
+        "text": full_text,
+        "config": config,
+        "pages_info": pages_info,
+    }
     return await proxy_post(f"{EMBED_URL}/{embed_type}/", body=param)
