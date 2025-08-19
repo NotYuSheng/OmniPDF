4a2238e47d7f1efecf1e07f4052723186344f216
Lim Dong Wan <limdongwan201528@gmail.com>
2025-08-19 16:22:40 +0800

    fix: Remove additional LLM URL used for local testing from env and openai_client

--- shared_utils/openai_client.py
+++ shared_utils/openai_client.py
@@ -7,6 +7,6 @@ def get_openai_client() -> AsyncOpenAI:
     Initialize and return an OpenAI client instance.
     """
     return AsyncOpenAI(
-        base_url=os.getenv("LLM_API_URL"), # Make sure `/v1` is included
+        base_url=os.getenv("OPENAI_BASE_URL"), # Make sure `/v1` is included
         api_key=os.getenv("OPENAI_API_KEY"),
     )
