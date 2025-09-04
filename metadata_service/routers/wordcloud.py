8826d7212b183b649c7163365e2a72a59cc48bc0
Lim Dong Wan <limdongwan201528@gmail.com>
2025-09-04 11:57:47 +0800

    chore: Manage newlines

--- metadata_service/routers/wordcloud.py
+++ metadata_service/routers/wordcloud.py
@@ -51,4 +51,5 @@ async def get_wordcloud(
         if not upload_fileobj(img_file, img_filepath):
             raise HTTPException(status_code=500, detail="failed to upload file")
     document_files.add(doc_id, img_filepath)
+    
     return WordcloudResponse(doc_id=doc_id, top_words=top_words)
